c0971e1187c9a179fa832cdf18a2f78cda7b5caf
John Ross <jlross42@gmail.com>
2024-12-01 22:37:30 -0500

    templates

---
+++ templates/puzzle.py
@@ -0,0 +1,17 @@
+class Puzzle:
+    def __init__(self, day: int):
+        self.day = day
+
+    def get_lines(self) -> list[str]:
+        with open(f"inputs/d{self.day:02}.in") as f:
+            data = f.read()
+        return data.splitlines()
+
+    def solve(self) -> None:
+        lines = self.get_lines()
+        for line in lines:
+            print(line)
+
+
+if __name__ == "__main__":
+    Puzzle(0).solve()
