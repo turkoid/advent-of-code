2016dd8daa6fb849881e764b78ab098b4bb930d3
John Ross <jlross42@gmail.com>
2024-12-07 13:04:24 -0500

    d04p2

--- y2024/d04p2.py
+++ y2024/d04p2.py
@@ -10,9 +10,18 @@ class Data(TypedDict):
 class Puzzle:
     __test__: Data = {
         "data": """
-
-    """,
-        "expected": None,
+            MMMSXXMASM
+            MSAMXMSMSA
+            AMXSXMAAMM
+            MSAMASMSMX
+            XMASAMXAMM
+            XXAMMXXAMA
+            SMSMSASXSS
+            SAXAMASAAA
+            MAMMMXMMMM
+            MXMXAXMASX
+        """,
+        "expected": 9,
     }
 
     def __init__(self, day: int | None = None, part: int | None = None):
@@ -26,21 +35,15 @@ class Puzzle:
 
     def get_data(self, path: str) -> str:
         with open(path) as f:
-            return f.read().strip()
+            return f.read()
 
-    def get_lines(self, data: str) -> list[list[str]]:
-        current_lines = []
-        line_groups = [current_lines]
-        for line in data.splitlines():
-            line = line.strip()
-            if line.startswith("#"):
-                continue
-            if len(line) == 0:
-                current_lines = []
-                line_groups.append(current_lines)
-            else:
-                current_lines.append(line)
-        return line_groups
+    def get_lines(self, data: str) -> list[str]:
+        lines = [
+            line.strip()
+            for line in data.splitlines()
+            if line.strip() and not line.startswith("#")
+        ]
+        return lines
 
     def get_input(self) -> str:
         return self.get_data(f"inputs/d{self.day:02}.in")
@@ -51,7 +54,18 @@ class Puzzle:
         return data, expected
 
     def solution(self, data: str) -> Any:
-        return None
+        lines = self.get_lines(data)
+        mas_sam = {"MAS", "SAM"}
+        count = 0
+        for y, line in enumerate(lines[1:-1], start=1):
+            for x, c in enumerate(line[1:-1], start=1):
+                if c != "A":
+                    continue
+                nw_se_diag = f"{lines[y - 1][x -1]}A{lines[y + 1][x + 1]}"
+                sw_ne_diag = f"{lines[y + 1][x - 1]}A{lines[y - 1][x + 1]}"
+                if nw_se_diag in mas_sam and sw_ne_diag in mas_sam:
+                    count += 1
+        return count
 
     def solve(self, test: bool | str = True) -> None:
         if isinstance(test, str):
