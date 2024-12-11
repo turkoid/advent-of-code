4f219a31e9e31b90d86f03426a87e65cefcc2417
John Ross <jlross42@gmail.com>
2024-12-11 09:46:07 -0500

    template changes

--- templates/puzzle.py
+++ templates/puzzle.py
@@ -1,18 +1,15 @@
+from collections.abc import Iterator
 from typing import Any
-from typing import TypedDict
-
-
-class Data(TypedDict):
-    data: str
-    expected: Any
 
 
 class Puzzle:
-    __test__: Data = {
-        "data": """
+    __test__ = {
+        "data": [
+            """
 
-    """,
-        "expected": None,
+            """,
+        ],
+        "expected": [],
     }
 
     def __init__(self, day: int | None = None, part: int | None = None):
@@ -48,10 +45,10 @@ class Puzzle:
     def get_input(self) -> str:
         return self.get_data(f"inputs/d{self.day:02}.in")
 
-    def get_test_data(self) -> tuple[str, str]:
-        data = self.__test__["data"].strip()
-        expected = self.__test__["expected"]
-        return data, expected
+    def get_test_data(self) -> Iterator[tuple[str, Any]]:
+        cfg = self.__test__
+        assert len(cfg["data"]) == len(cfg["expected"])
+        return zip([data.strip() for data in cfg["data"]], cfg["expected"])
 
     def solution(self, data: str) -> Any:
         return None
@@ -61,9 +58,9 @@ class Puzzle:
             print(self.solution(test))
             return
         if test:
-            data, expected = self.get_test_data()
-            solution = self.solution(data)
-            assert solution == expected, f"solution != result: {solution} != {expected}"
+            for i, (data, expected) in enumerate(self.get_test_data()):
+                solution = self.solution(data)
+                assert solution == expected, f"[{i}] --- solution != result: {solution} != {expected}"
         print(self.solution(self.get_input()))
 
 
