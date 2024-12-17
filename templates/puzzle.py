3372c0da97eeda553f0f0b2d0f8bb1dd2a8e82c8
John Ross <jlross42@gmail.com>
2024-12-17 07:03:15 -0500

    template updates

--- templates/puzzle.py
+++ templates/puzzle.py
@@ -5,15 +5,6 @@ from typing import Self
 
 
 @dataclass
-class Point:
-    x: int
-    y: int
-
-    def __repr__(self):
-        return f"({self.x}, {self.y})"
-
-
-@dataclass
 class Object:
     @classmethod
     def from_data(cls, data: str) -> Self:
