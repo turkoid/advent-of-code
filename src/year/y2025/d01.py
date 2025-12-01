d1a56a3a145280478956bfbd71821d3c4add34fd
John Ross <jlross42@gmail.com>
2025-12-01 13:10:55 -0500

    2025 day 1

---
+++ src/year/y2025/d01.py
@@ -0,0 +1,39 @@
+from templates.puzzle import Puzzle
+
+
+class Day1Part1(Puzzle[list[int], int]):
+    def parse_data(self, data: str) -> list[int]:
+        instructions = self.get_lines(data)
+        rotations = []
+        for instruction in instructions:
+            direction = instruction[0]
+            distance = int(instruction[1:])
+            rotations.append(distance if direction == "R" else -distance)
+        return rotations
+
+    def solution(self, parsed_data: list[int]) -> int:
+        dial = 50
+        zero_click = 0
+        for rotation in parsed_data:
+            dial += rotation
+            dial %= 100
+            if dial == 0:
+                zero_click += 1
+        return zero_click
+
+
+class Day1Part2(Day1Part1):
+    def solution(self, parsed_data: list[int]) -> int:
+        dial = 50
+        zero_click = 0
+        for rotation in parsed_data:
+            full_rotations = int(rotation / 100)
+            zero_click += abs(full_rotations)
+            rotation -= full_rotations * 100
+            if rotation == 0:
+                continue
+            dial += rotation
+            if dial != rotation and (dial <= 0 or dial >= 100):
+                zero_click += 1
+            dial %= 100
+        return zero_click
