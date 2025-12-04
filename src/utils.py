25c1d672b9f61b5e17f1aedc1a2b23823d08772f
John Ross <jlross42@gmail.com>
2025-12-04 03:44:42 -0500

    pretty print grid enhancements

--- src/utils.py
+++ src/utils.py
@@ -6,8 +6,26 @@ def root_dir() -> Path:
     return Path(__file__).parent.parent
 
 
-def pretty_grid(self, grid: list[list[Any] | Any]) -> str:
-    return "\n".join("".join(str(o) for o in line) if isinstance(line, list) else str(line) for line in grid)
+def pretty_grid(grid: list[list | Any], padding: int = 1) -> str:
+    padding = max(padding, 0)
+    lines = []
+    if padding:
+        h_pad = " " * padding
+        v_pad = " " * (padding * 2 + len(grid[0]))
+        lines.extend([v_pad] * padding)
+    else:
+        h_pad = ""
+        v_pad = ""
+    for row in grid:
+        if isinstance(row, list):
+            row = "".join(str(o) for o in row)
+        else:
+            row = str(row)
+        lines.append(f"{h_pad}{row}{h_pad}")
+    if padding:
+        lines.extend([v_pad] * padding)
+
+    return "\n".join(lines)
 
 
 def create_banner(msg: str) -> str:
