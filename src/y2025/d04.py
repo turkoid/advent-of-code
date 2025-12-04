d779707dc5296bcc0f5f8babe04e4f9290688f0d
John Ross <jlross42@gmail.com>
2025-12-04 04:30:22 -0500

    small refactor of part1

--- src/y2025/d04.py
+++ src/y2025/d04.py
@@ -9,30 +9,36 @@ type Grid = list[list[str]]
 class Day4Part1(Puzzle):
     def parse_data(self, data: str) -> Grid:
         grid = [list(line) for line in self.get_input_lines(data)]
+        assert len(grid) > 0
+        width = len(grid[0])
+        assert all(len(row) == width for row in grid)
         return grid
 
-    def solution(self, parsed_data: Grid) -> int:
-        grid = parsed_data
-        solution_grid = copy.deepcopy(parsed_data)
-        width = len(grid[0])
-        height = len(grid)
-        self.log(pretty_grid(grid))
-        moveable_paper_rolls = 0
-        for y, row in enumerate(grid):
+    def move_paper_rolls(self, warehouse: Grid) -> int:
+        warehouse = copy.deepcopy(warehouse)
+        width = len(warehouse[0])
+        height = len(warehouse)
+        moved_paper_rolls = 0
+        for y, row in enumerate(warehouse):
             for x, cell in enumerate(row):
                 if cell != "@":
                     continue
                 adjacent_cells = []
                 if y - 1 >= 0:
-                    adjacent_cells.extend(grid[y - 1][max(x - 1, 0) : min(x + 2, width + 1)])
+                    adjacent_cells.extend(warehouse[y - 1][max(x - 1, 0) : min(x + 2, width + 1)])
                 if x - 1 >= 0:
                     adjacent_cells.append(row[x - 1])
                 if x + 1 < width:
                     adjacent_cells.append(row[x + 1])
                 if y + 1 < height:
-                    adjacent_cells.extend(grid[y + 1][max(x - 1, 0) : min(x + 2, width + 1)])
-                if adjacent_cells.count("@") < 4:
-                    moveable_paper_rolls += 1
-                    solution_grid[y][x] = "X"
-        self.log(pretty_grid(solution_grid))
-        return moveable_paper_rolls
+                    adjacent_cells.extend(warehouse[y + 1][max(x - 1, 0) : min(x + 2, width + 1)])
+                if (adjacent_cells.count("@") + adjacent_cells.count("X")) < 4:
+                    moved_paper_rolls += 1
+                    warehouse[y][x] = "X"
+        self.log(pretty_grid(warehouse))
+        return moved_paper_rolls
+
+    def solution(self, parsed_data: Grid) -> int:
+        self.log(pretty_grid(parsed_data))
+        moved_paper_rolls = self.move_paper_rolls(parsed_data)
+        return moved_paper_rolls
