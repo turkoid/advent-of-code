71f25e5229b949c2fefa2bdc1c138da55c4a6942
John Ross <jlross42@gmail.com>
2025-12-06 14:28:54 -0500

    day6p2 done

--- src/y2025/d06.py
+++ src/y2025/d06.py
@@ -19,9 +19,20 @@ class Day6Part1(Puzzle):
         return sum
 
 
-class Day6Part2(Puzzle):
-    def parse_data(self, data: str) -> None:
-        pass
-
-    def solution(self, parsed_data: None) -> None:
-        pass
+class Day6Part2(Day6Part1):
+    def parse_data(self, data: str) -> tuple[list[list[int]], list[str]]:
+        lines = self.get_input_lines(data)
+        self.log("\n".join(lines))
+        ops = lines[-1].split()
+        cols = [("".join(line)).strip() for line in zip(*lines[:-1])]
+        cols.append("")
+        vals = []
+        group = []
+        for col in cols:
+            if col == "":
+                vals.append([int(v) for v in group])
+                group = []
+            else:
+                group.append(col)
+        assert len(vals) == len(ops)
+        return vals, ops
